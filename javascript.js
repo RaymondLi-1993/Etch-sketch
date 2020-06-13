d860e34c4cb7dba93b05c4f26527de9eb5249607
Raymond Li <Rlii0303@gmail.com>
2020-06-13 00:25:24 -0700

    updated

--- javascript.js
+++ javascript.js
@@ -1,24 +1,68 @@
 const BOARD = document.querySelector(`#draw`)
-const HEIGHT = 16;
-const WIDTH = 16;
+const RESET = document.querySelector(`#reset`);
+const GRID = document.querySelector(`#grid-size`);
+const RANDOM = document.querySelector(`#color-change`);
 
 
-let add = function(event){
-  let target = event.target;
-  target.setAttribute('class', 'red');
+let change = (event) =>{
+  if(event.target.classList.value.includes(`color`)){
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  event.target.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+  }
+  else{
+  event.target.style.backgroundColor = `#FFFAFA`
+  }
 }
 
-for (let i = 0; i < HEIGHT; ++i){
-  const ROWS = document.createElement(`div`);
-  ROWS.setAttribute('id', 'rows');
-    for(let j = 0; j < WIDTH; ++j){
-      const COLUMNS = document.createElement('div');
-      COLUMNS.setAttribute('id', 'columns');
-      COLUMNS.addEventListener(`mouseover`, add);
-      ROWS.appendChild(COLUMNS);
-    } 
-    BOARD.appendChild(ROWS);
+BOARD.addEventListener(`mouseover`, change);
+RESET.addEventListener('click', () =>{
+  location.reload();
+})
+
+
+let makeGrid = (size = 16) => {
+  BOARD.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  for (let i = 0; i < size; ++i){
+    for (let j = 0; j < size; ++j){
+      const CELL = document.createElement('div');
+      CELL.classList.add(`cell`);
+      CELL.style = `background-color : silver`;
+      BOARD.appendChild(CELL);
+    }
+  }
 }
+
+let clearGrid = () =>{
+  let container = document.querySelector(`#draw`);
+  while (container.firstElementChild){
+    container.removeChild(container.lastChild);
+  }
+}
+
+
+GRID.addEventListener('click', () =>{
+let result = prompt('Please enter a number between 4-24!');
+let newResult = +result;
+  if((newResult >= 4 && newResult <= 24)){
+    clearGrid();
+    makeGrid(newResult);
+  }
+  else{
+  alert(`${result} is not a valid paremeter!`);
+  }
+})
+
+RANDOM.addEventListener('click', ()=>{
+  let cell = document.querySelectorAll('.cell')
+  cell.forEach((elem)=>{
+    elem.classList.add('color');
+  })
+});
+
+makeGrid();
+
 
 
 
